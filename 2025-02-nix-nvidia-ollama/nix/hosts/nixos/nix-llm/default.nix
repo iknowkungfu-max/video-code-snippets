cbb8fe50961bbae19ce71ebe23f4c79bf8348a15
iknowkungfu-max <iknowkungfu@gmail.com>
2025-08-08 18:04:26 -0400

    Update default.nix

--- 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
+++ 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
@@ -17,16 +17,13 @@
   # Network configuration
   networking = {
     firewall.enable = false;
-    hostName = "nix-llm";
+    hostName = "nix-ollama";
     interfaces.ens18 = {
-      useDHCP = false;
-      ipv4.addresses = [{
-        address = "10.42.37.100";
-        prefixLength = 24;
+      useDHCP = true;
       }];
     };
-    defaultGateway = "10.42.37.254";
-    nameservers = [ "10.42.37.254" ];
+    defaultGateway = "192.168.1.1";
+    nameservers = [ "192.168.1.1" ];
   };
 
   # System localization
